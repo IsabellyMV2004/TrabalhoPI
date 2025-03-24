323887a437c12bd5ceab8b293f6dd2bbf2331107
IsabellyMV2004 <isabellymarafaovoltareli2004@gmail.com>
2025-03-24 09:26:32 -0300

    id

--- frontend/src/Telas/TelaCadastro/telaCadastro.jsx
+++ frontend/src/Telas/TelaCadastro/telaCadastro.jsx
@@ -8,16 +8,16 @@ export default function TelaCadastro() {
                 <h1>Cadastro de Funcionario</h1>
                 
                 <label>Nome</label>
-                <input type="text" placeholder="Informe o nome completo" />
+                <input type="text" placeholder="Informe o nome completo" id="nome"/>
                 
                 <label>CPF</label>
-                <input type="text" placeholder="000.000.000-00" />
+                <input type="text" placeholder="000.000.000-00" id="cpf"/>
                 
                 <label>Cargo</label>
-                <input type="text" placeholder="Informe o cargo" />
+                <input type="text" placeholder="Informe o cargo" id="cargo"/>
                 
                 <label>Nivel de Acesso</label>
-                <input type="text" placeholder="MédInforme o nivel" />
+                <input type="text" placeholder="MédInforme o nivel" id="nivel"/>
                 
                 <button>CONTINUAR</button>
             </div>
